76441953d0d88ce4d22536d63dd2e67a1bd81aec
Mohit Sharma <oyemohit12@gmail.com>
2025-10-02 19:34:03 +0530

    Every possible combination of 1, 2, and 3.

---
+++ codes/combination.java
@@ -0,0 +1,8 @@
+public class combination {
+	public static void main(String args[]) {
+		for(int i = 1; i < 4; i++)
+			for(int j = 1; j < 4; j++)
+				for(int k = 1; k < 4; k++)
+					System.out.println(i+"\t"+j+"\t"+k);
+	}
+}
